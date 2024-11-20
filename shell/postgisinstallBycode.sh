9ba658b830d743ac76e86a4c53f2b40f8c3cd565
xiabingbao <xiabingbao2023@outlook.com>
2024-11-20 11:49:09 +0800

    吃饭前提交一下

--- shell/postgisinstallBycode.sh
+++ shell/postgisinstallBycode.sh
@@ -4,6 +4,16 @@ if [[ $EUID -ne 0 ]]; then
     echo "请以 root 用户身份运行此脚本。"
     exit 1
 fi
+#通过yum安装一些依赖
+yum install bzip2 -y -q
+#下载一些构建工具
+#安装cmake
+mkdir -p /opt/CMake
+cd /opt/source/nginx
+echo "正在下载cmake"
+wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4-Linux-x86_64.tar.gz 2>&1 | grep '%'
+tar -zxvf cmake-3.13.4-Linux-x86_64.tar.gz
+#下载各种资源
 wget https://download.osgeo.org/postgis/source/postgis-3.5.0.tar.gz
 tar -xvf postgis-3.5.0.tar.gz
 cd postgis-3.5.0
@@ -18,7 +28,6 @@ make -s -j$(nproc)
 make install
 #安装geos（postgis依赖于它）
 wget https://download.osgeo.org/geos/geos-3.12.2.tar.bz2
-yum install -y bzip2
 tar -jxf geos-3.12.2.tar.bz2
 cd geos-3.12.2
 mkdir _build
@@ -26,9 +35,6 @@ cd _build
 /root/cmake-3.13.4-Linux-x86_64/bin/cmake     -DCMAKE_BUILD_TYPE=Release   /root/geos-3.12.2
 make -s -j$(nproc)
 make install
-#安装cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4-Linux-x86_64.tar.gz
-tar -zxvf cmake-3.13.4-Linux-x86_64.tar.gz
 #安装gcc
 yum install scl-utils -y
 sudo yum install centos-release-scl -y
