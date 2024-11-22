de659057737021b471b98e49d41caddaf6ccb5d9
xiabingbao <xiabingbao2023@outlook.com>
2024-11-22 16:41:19 +0800

    完成postgis所需要全部功能的编译脚本配置

--- shell/postgisinstallBycode.sh
+++ shell/postgisinstallBycode.sh
@@ -4,41 +4,23 @@ if [[ $EUID -ne 0 ]]; then
     echo "请以 root 用户身份运行此脚本。"
     exit 1
 fi
-#通过yum安装一些依赖
+echo "正在通过yum安装一些依赖"
 yum install bzip2 python-devel centos-release-scl -y -q
-sed -e "s|^mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-SCLo-scl.repo
-sed -e "s|^baseurl=http://mirror.centos.org/centos/|baseurl=https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009|g" /etc/yum.repos.d/CentOS-SCLo-scl.repo
-scl-utils
-#下载一些构建工具
-#安装gcc
 sudo sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-    -e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009|g" \
-    -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009|g" \
+    -e "s|^# baseurl=http://mirror.centos.org/centos/7|baseurl=https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009|g" \
+    -e "s|^#baseurl=http://mirror.centos.org/centos/7|baseurl=https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009|g" \
     -i.bak \
-    /etc/yum.repos.d/CentOS-*.repo
-yum install  -y
-sudo yum install  -y
-yum install devtoolset-7 -y
+    /etc/yum.repos.d/CentOS-SCLo-*.repo
+echo "安装了sclo的yum源并替换源为https://mirrors.bfsu.edu.cn/centos-vault/7.9.2009"
+echo "该源提供了更高版本的gcc"
+#安装高版本gcc
+yum install devtoolset-7 -y -q
 source /opt/rh/devtoolset-7/enable
 #安装cmake
 mkdir -p /opt/CMake
 echo "正在下载和安装cmake"
 wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4-Linux-x86_64.tar.gz -O /opt/CMake/cmake.tar.gz 2>&1 | grep '%'
 tar -zxf /opt/CMake/cmake.tar.gz -C /opt/CMake/
-#下载各种资源
-mkdir -p /opt/src/postgis
-wget https://download.osgeo.org/postgis/source/postgis-3.5.0.tar.gz
-tar -xvf postgis-3.5.0.tar.gz
-cd postgis-3.5.0
-./configure --with-pgconfig=/opt/postgresql/pg12.20server/bin/pg_config --with-xml2config=/opt/postgis/libxml2/bin/xml2-config --with-geosconfig=/opt/postgis/geos/bin/geos-config --without-protobuf
-# 安装libxml2（postgis依赖于它）
-wget https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.4.tar.xz -O /opt/postgis/libxml2-2.13.4.tar.xz 2>&1 | grep '%'
-tar -xf /opt/postgis/libxml2-2.13.4.tar.xz -C  /opt/postgis/
-cd /opt/postgis/libxml2-2.13.4
-source /opt/rh/devtoolset-7/enable
-./configure
-make -s -j$(nproc)
-make install
 #安装geos（postgis依赖于它）
 wget https://download.osgeo.org/geos/geos-3.12.2.tar.bz2 -O /opt/postgis/geos.tar.bz2 2>&1 | grep '%'
 tar -jxf /opt/postgis/geos.tar.bz2 -C  /opt/postgis/
@@ -48,8 +30,6 @@ cd /opt/postgis/geos-3.12.2/_build
 source /opt/rh/devtoolset-7/enable
 make -s -j$(nproc)
 make install
-
-
 #安装sqlite3（proj依赖它）
 wget https://www.sqlite.org/2022/sqlite-autoconf-3400000.tar.gz  -O /opt/postgis/sqllite.tar.gz 2>&1 | grep '%'
 tar -zxf /opt/postgis/sqllite.tar.gz -C  /opt/postgis/
@@ -67,6 +47,7 @@ source /opt/rh/devtoolset-7/enable
 /opt/CMake/cmake-3.13.4-Linux-x86_64/bin/cmake ..
 make -s -j$(nproc)
 make install
+#编译出错
 #安装gdal（postgis依赖它）
 wget https://github.com/OSGeo/gdal/releases/download/v3.6.1/gdal-3.6.1.tar.gz -O  /opt/postgis/gdal.tar.gz 2>&1 | grep '%'
 tar -zxf /opt/postgis/gdal.tar.gz -C  /opt/postgis/
@@ -76,6 +57,14 @@ source /opt/rh/devtoolset-7/enable
 /opt/CMake/cmake-3.13.4-Linux-x86_64/bin/cmake ..
 #不要使用多线程编译 会导致编译失败
 make
+make install
+# 安装libxml2（postgis依赖于它）
+wget https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.4.tar.xz -O /opt/postgis/libxml2-2.13.4.tar.xz 2>&1 | grep '%'
+tar -xf /opt/postgis/libxml2-2.13.4.tar.xz -C  /opt/postgis/
+cd /opt/postgis/libxml2-2.13.4
+source /opt/rh/devtoolset-7/enable
+./configure
+make -s -j$(nproc)
 make install
 #安装protobuf（postgis依赖它同时protobuf-c依赖它）
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protobuf-all-3.11.4.tar.gz -O  /opt/postgis/protobuf.tar.gz 2>&1 | grep '%'
@@ -93,13 +82,22 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 ./configure
 make -s -j$(nproc)
 make install
-#TODO 编译PCRE
 #编译pcre
-
+wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.41/pcre2-10.41.tar.gz -O  /opt/postgis/pcre2.tar.gz 2>&1 | grep '%'
+tar -zxf  /opt/postgis/pcre2.tar.gz  -C  /opt/postgis/
+cd /opt/postgis/pcre2-10.41/
+./configure --enable-utf8
+make -s -j$(nproc)
+make install
 #TODO 编译json-c
 #编译json-c
-
-
+wget https://github.com/json-c/json-c/archive/refs/tags/json-c-0.18-20240915.tar.gz -O  /opt/postgis/json-c.tar.gz 2>&1 | grep '%'
+tar -zxf  /opt/postgis/json-c.tar.gz  -C  /opt/postgis/
+mkdir -p /opt/postgis/json-c-json-c-0.18-20240915/build
+cd /opt/postgis/json-c-json-c-0.18-20240915/build
+/opt/CMake/cmake-3.13.4-Linux-x86_64/bin/cmake ..
+make -s -j$(nproc)
+make install
 #编译postgis
 wget https://download.osgeo.org/postgis/source/postgis-3.5.0.tar.gz  -O  /opt/postgis/postigs.tar.gz 2>&1 | grep '%'
 tar -zxf /opt/postgis/postigs.tar.gz -C  /opt/postgis/
